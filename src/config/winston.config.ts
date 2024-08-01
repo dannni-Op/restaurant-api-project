62ee20b94009608434eb0e96b8199d56c1c29615
lord <lord@lord>
2024-08-01 19:26:59 +0700

    feat: setting winston logger

--- src/config/winston.config.ts
+++ src/config/winston.config.ts
@@ -1,8 +1,13 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WinstonModuleAsyncOptions } from 'nest-winston';
+import * as winston from 'winston';
 
 export const winstonConfig: WinstonModuleAsyncOptions = {
   imports: [ConfigModule],
-  useFactory: () => ({}),
+  useFactory: (configService: ConfigService) => ({
+    format: winston.format.json(),
+    level: configService.get('LOG') || 'debug',
+    transports: [new winston.transports.Console()],
+  }),
   inject: [ConfigService],
 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
   await app.listen(configService.get<number>('DB_PORT') ?? 3000);
 }
 bootstrap();
